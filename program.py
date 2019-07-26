7eb8480f8a870313f62e98cfac1c14a890b7ca51
DKotaroD <44012594+DKotaroD@users.noreply.github.com>
2019-07-26 13:16:37 +0900

    Create program.py

---
+++ program.py
@@ -0,0 +1,9 @@
+def has99(x):
+    num = []
+    for i in enumerate(x):
+        if i[1] == 9:
+            num.append(i[0])
+    if num[0] + 1 == num[1]:
+        return True
+    else:
+        return False
